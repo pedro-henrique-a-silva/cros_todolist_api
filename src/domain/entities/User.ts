40e5f79d17ab0ac2bbfdbce746d1fc1e1aeda96f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-04 18:35:15 -0300

    feat: create user entity

---
+++ src/domain/entities/User.ts
@@ -0,0 +1,56 @@
+import { UserInterface } from '../interfaces/User'
+
+export default class User {
+  private _id: string
+  private _name: string
+  private _email: string
+  private _password: string
+
+  constructor(id: string, name: string, email: string, password: string) {
+    this._id = id
+    this._name = name
+    this._email = email
+    this._password = password
+  }
+
+  get id(): string {
+    return this._id
+  }
+
+  set id(id: string) {
+    this._id = id
+  }
+
+  get name(): string {
+    return this._name
+  }
+
+  set name(name: string) {
+    this._name = name
+  }
+
+  get email(): string {
+    return this._email
+  }
+
+  set email(email: string) {
+    this._email = email
+  }
+
+  get password(): string {
+    return this._password
+  }
+
+  set password(password: string) {
+    this._password = password
+  }
+
+  toCreate(): UserInterface {
+    return {
+      id: this._id,
+      name: this._name,
+      email: this._email,
+      password: this._password,
+    }
+  }
+}
