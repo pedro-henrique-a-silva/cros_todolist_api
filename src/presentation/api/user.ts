7106bb87580e49d795cc8f2f294ade251eaa4a47
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-04 20:01:13 -0300

    feat: creating login route

--- src/presentation/api/user.ts
+++ src/presentation/api/user.ts
@@ -1,22 +1,33 @@
 import { Request, Response, Router } from 'express'
-import { toCreateScheme } from '../schemes/userSchemes'
+import { toCreateScheme, toLoginScheme } from '../schemes/userSchemes'
 import UserDomain from '../../domain/services/UserDomain'
-import CreateUserException from '../exceptions/CreateUserException'
-
-const userRouter = Router()
+import BadRequestException from '../exceptions/BadRequestException'
 
 const userDomain = new UserDomain()
+const userRouter = Router()
 
 userRouter.post('/create', async (req: Request, res: Response) => {
   const createData = toCreateScheme.safeParse(req.body)
 
   if (!createData.success) {
-    throw new CreateUserException(createData.error.errors[0].message)
+    throw new BadRequestException(createData.error.errors[0].message)
   }
 
   const createdUser = await userDomain.createUser(createData.data)
 
-  res.status(201).send(createdUser)
+  return res.status(201).json(createdUser)
+})
+
+userRouter.post('/login', async (req: Request, res: Response) => {
+  const loginData = toLoginScheme.safeParse(req.body)
+
+  if (!loginData.success) {
+    throw new BadRequestException(loginData.error.errors[0].message)
+  }
+
+  const userAuthenticated = await userDomain.login(loginData.data)
+
+  return res.status(200).json(userAuthenticated)
 })
 
 export default userRouter
