752e061ef61fbb33f69130636a7fe22dda4e4807
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-04 15:07:17 -0300

    fix: fixing port of api

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const port = process.env.APP_PORT || 3000
+const port = process.env.APP_PORT || 3001
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
