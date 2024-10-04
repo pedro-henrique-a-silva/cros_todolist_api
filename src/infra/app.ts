d32ff3549df5625f993f3359c2f5e0d54daa9e1f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-04 20:02:41 -0300

    feat: create error middleware

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,11 +1,28 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import routes from '../presentation/api'
+import CustomException from '../domain/exceptions/CustomException'
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Hello World')
+app.get('/', (_req: Request, res: Response) => {
+  return res.status(200).send('Hello World')
+})
+
+app.use(routes)
+
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof CustomException) {
+    return res.status(err.statusCode).json({
+      errorDescription: err.message,
+    })
+  }
+
+  return res.status(500).json({
+    errorCode: 'internal_server_error',
+    message: 'Opss! Ocorreu um erro',
+  })
 })
 
 export default app
