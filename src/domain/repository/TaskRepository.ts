8bb1ee94eca1a357fad68ef392070299a6c03811
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-05 12:23:28 -0300

    feat: Add method createSubTask to interface TaskDomainInterface

--- src/domain/repository/TaskRepository.ts
+++ src/domain/repository/TaskRepository.ts
@@ -5,4 +5,5 @@ export default interface TaskRepository {
   findAllTasks(userId: string): Promise<Task[]>
   updateTask(task: Task): Promise<Task>
   findAllSubtasks(id: string, userId: string): Promise<Task[]>
+  createSubTask(taskData: Task): Promise<Task>
 }
