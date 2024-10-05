eb29a2fc214f815336e100a5e7e20ab1e191f436
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-05 11:46:30 -0300

    feat: Add updateTask method to TaskRepository interface

--- src/domain/repository/TaskRepository.ts
+++ src/domain/repository/TaskRepository.ts
@@ -3,4 +3,5 @@ import Task from '../entities/Task'
 export default interface TaskRepository {
   createTask(taskData: Task): Promise<Task>
   findAllTasks(userId: string): Promise<Task[]>
+  updateTask(task: Task): Promise<Task>
 }
