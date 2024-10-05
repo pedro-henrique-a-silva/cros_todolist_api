5cecdb454f78789907b8502bfa0d34e729b9284a
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-05 11:49:55 -0300

    feat: Add scheme for updating a task

--- src/presentation/schemes/taskSchemes.ts
+++ src/presentation/schemes/taskSchemes.ts
@@ -4,3 +4,13 @@ export const toCreateTaskScheme = z.object({
   title: z.string(),
   content: z.string(),
 })
+
+export const toUpdateTaskScheme = z.object({
+  title: z.string(),
+  content: z.string(),
+  status: z.enum(['PENDING', 'DONE']),
+})
+
+export const idParamScheme = z.object({
+  id: z.string().uuid(),
+})
