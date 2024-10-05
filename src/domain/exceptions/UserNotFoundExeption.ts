0204f6404acd8324aeddd40be2b3ff93d90f86cd
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-05 16:21:30 -0300

    Fix: UserNotFoundExeption status code

--- src/domain/exceptions/UserNotFoundExeption.ts
+++ src/domain/exceptions/UserNotFoundExeption.ts
@@ -1,7 +1,7 @@
 import CustomException from './CustomException'
 
 export default class UserNotFoundExeption extends CustomException {
-  protected _statusCode: number = 404
+  protected _statusCode: number = 400
   constructor(message: string) {
     super(message)
     this.name = 'UserNotFoundExeption'
