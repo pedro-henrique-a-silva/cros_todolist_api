b84b9560edce6a0c5bfdd148536dddbb5374417c
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-05 00:02:52 -0300

    feat: add taskRouter to API index

--- src/presentation/api/index.ts
+++ src/presentation/api/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import userRouter from './user'
+import taskRouter from './tasks'
 
 const router = Router()
 
 router.use('/user', userRouter)
+
+router.use('/task', taskRouter)
 
 export default router
