ceebeefebee5283ef17898987e51c3515ed376b6
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-05 12:05:31 -0300

    feat: Add findAllSubtasks to TaskRepository interface

--- src/domain/repository/TaskRepository.ts
+++ src/domain/repository/TaskRepository.ts
@@ -4,4 +4,5 @@ export default interface TaskRepository {
   createTask(taskData: Task): Promise<Task>
   findAllTasks(userId: string): Promise<Task[]>
   updateTask(task: Task): Promise<Task>
+  findAllSubtasks(id: string, userId: string): Promise<Task[]>
 }
