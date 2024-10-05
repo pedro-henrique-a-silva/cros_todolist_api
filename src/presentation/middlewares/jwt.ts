9092b881fd4711f2101cae16320df1fa571f16f2
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-10-05 00:03:51 -0300

    feat: Add middleware to validate JWT

---
+++ src/presentation/middlewares/jwt.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Response } from 'express'
+import JWTDomain from '../../domain/services/JwtDomain'
+import BadRequestException from '../exceptions/BadRequestException'
+import { jwtScheme } from '../schemes/jwtScheme'
+import { RequestWitJwt } from '../../domain/interfaces/Task'
+
+export const jwtValidator = (
+  req: RequestWitJwt,
+  res: Response,
+  next: NextFunction,
+) => {
+  const tokenData = jwtScheme.safeParse(req.headers)
+
+  if (!tokenData.success) {
+    throw new BadRequestException('Token not provided')
+  }
+
+  try {
+    const decoded = JWTDomain.verify(tokenData.data.authorization.split(' ')[1])
+
+    req.user = {
+      name: decoded?.name,
+      email: decoded?.email,
+    }
+    return next()
+  } catch {
+    throw new BadRequestException('Token not provided')
+  }
+}
